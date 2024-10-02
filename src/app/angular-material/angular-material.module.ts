2e6cec62a0362cef45ca719cdc63a1ab8fb27c92
ismaelp96 <ismael6991@live.com>
2024-10-02 14:24:56 -0300

    style: Criação do Layout do componente de detalhes

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  imports: [MatButtonModule, MatDividerModule, MatIconModule],
-  exports: [MatButtonModule, MatDividerModule, MatIconModule],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule, MatListModule],
+  exports: [MatButtonModule, MatDividerModule, MatIconModule, MatListModule],
 })
 export class AngularMaterialModule {}
