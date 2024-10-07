207fbb60120847214af800babaa794e4d84a7b65
ismaelp96 <ismael6991@live.com>
2024-10-07 16:59:13 -0300

    feat: lógica de filtro por nome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,12 +12,14 @@ import { IfilterOptions } from './interfaces/filter-options.interface';
 export class AppComponent implements OnInit {
   usersList: IUser[] = [];
   userSelected: IUser = {} as IUser;
+  usersListFiltered: IUser[] = [];
   showUserDetails: boolean = false;
 
   ngOnInit() {
     setTimeout(() => {
       this.usersList = UsersList;
-    }, 1000);
+      this.usersListFiltered = this.usersList;
+    }, 1);
   }
 
   onUserSelected(user: IUser) {
@@ -27,5 +29,30 @@ export class AppComponent implements OnInit {
 
   onFilter(filterOptions: IfilterOptions) {
     console.log(filterOptions);
+    this.usersListFiltered = this.filterUsersList(
+      filterOptions,
+      this.usersList
+    );
+  }
+
+  filterUsersList(filterOptions: IfilterOptions, usersList: IUser[]): IUser[] {
+    let filteredList: IUser[] = [];
+
+    filteredList = this.filterUsersListByName(filterOptions.name, usersList);
+
+    return filteredList;
+  }
+
+  filterUsersListByName(name: string | null, usersList: IUser[]): IUser[] {
+    const NAME_NOT_TYPPED = name === null;
+    if (NAME_NOT_TYPPED) {
+      return usersList;
+    }
+
+    const filteredLIst = usersList.filter((user) =>
+      user.nome.toLocaleLowerCase().includes(name.toLocaleLowerCase())
+    );
+
+    return filteredLIst;
   }
 }
