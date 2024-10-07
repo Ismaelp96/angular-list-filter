6fdfeee30779def9d1370f1fad1dceded2d39223
ismaelp96 <ismael6991@live.com>
2024-10-07 08:25:13 -0300

    feat: pipe adress

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { PhonePipe } from './phone.pipe';
+import { AdressPipe } from './adress.pipe';
 
-@NgModule({ declarations: [PhonePipe], exports: [PhonePipe] })
+@NgModule({
+  declarations: [PhonePipe, AdressPipe],
+  exports: [PhonePipe, AdressPipe],
+})
 export class PipesModule {}
